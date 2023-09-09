70659a88f38ea0658f8736c9daea2d592d7cec15
IbraheemHaseeb7 <ibraheemhaseeb7@gmail.com>
2023-09-09 12:34:50 +0500

    completed the first exercise

--- exercise1/exercise1.r
+++ exercise1/exercise1.r
@@ -1,6 +1,15 @@
-library(tidyverse)
 
-# Importing using read_csv function
+library(dplyr)
+library(tidyr)
+
 students <- read.csv(file = "https://raw.githubusercontent.com/MicrosoftDocs/ml-basics/master/data/grades.csv")
 
-filter(students, StudyHours > 12, Grade > 80)
+
+# Replace NA in column StudyHours with the mean study hours
+students <- students %>% 
+  drop_na()
+
+results <- students %>%
+  mutate(Status = if_else(Grade >= 60, "PASS", "FAIL"))
+
+results %>% arrange(desc(Grade))
